6fb771503a7758c159907d2da9638c458d96de26
lewisolden5 <39215550-lewisolden5@users.noreply.replit.com>
2025-01-29 18:12:33 +0000

    Agent query: Can you access the smart contracts quiz page by clicking the 'Take Quiz' button at the end of the smart contracts content?
    Add smart contracts quiz component and route.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/b1009c5f-b85e-4734-ae17-fcd3dc4008d3/3cc7e4e7-5a49-4bc8-8b91-fc02d167412f.jpg

--- client/src/pages/modules/module2/Module2Routes.tsx
+++ client/src/pages/modules/module2/Module2Routes.tsx
@@ -5,6 +5,7 @@ import BlockchainBasics from './blockchain-basics';
 import DistributedLedger from './distributed-ledger';
 import ConsensusMechanisms from './consensus-mechanisms';
 import SmartContracts from './smart-contracts';
+import SmartContractsQuiz from './smart-contracts-quiz';
 
 const Module2Routes = () => {
   return (
@@ -14,6 +15,7 @@ const Module2Routes = () => {
       <Route path="/modules/module2/distributed-ledger" component={DistributedLedger} />
       <Route path="/modules/module2/consensus-mechanisms" component={ConsensusMechanisms} />
       <Route path="/modules/module2/smart-contracts" component={SmartContracts} />
+      <Route path="/modules/module2/smart-contracts-quiz" component={SmartContractsQuiz} />
     </Switch>
   );
 };
