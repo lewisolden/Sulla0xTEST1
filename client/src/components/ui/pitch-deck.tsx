ef8becc93e7565bf1d691baa1656f8351f5f79aa
lewisolden5 <39215550-lewisolden5@users.noreply.replit.com>
2025-02-13 17:42:21 +0000

    Agent query: Could you check if the platform infrastructure slide appears correctly as slide 13 in the deck? Please navigate through the slides to verify the order.
    Reorder slides in pitch deck
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/978a5dde-28d4-49e6-9e38-3e475bd1f351/2bb6cb2c-fb95-4f2a-b1aa-bb44fb63e9a7.jpg

--- client/src/components/ui/pitch-deck.tsx
+++ client/src/components/ui/pitch-deck.tsx
@@ -34,28 +34,28 @@ const PitchDeck: React.FC = () => {
   const slides = [
     titleSlide,                    // 1
     problemSlide,                  // 2
-    solutionSlide1,               // 3
-    solutionSlide2,               // 4
-    missionSlide,                 // 5
-    marketSlide1,                 // 6
-    marketSlide2,                 // 7
-    productSlide1,                // 8
-    productSlide2,                // 9
-    modulesSlide,                 // 10
-    roadmapSlide,                 // 11
-    progressSlide,                // 12
-    platformInfrastructureSlide,  // 13
-    futureFinanceSlide,           // 14
-    gtmSlide1,                    // 15
-    gtmSlide2,                    // 16
-    tokenSlide,                   // 17
-    tractionSlide,                // 18
-    financialSlide,               // 19
-    financialModelSlide,          // 20
-    fundingNarrativeSlide,        // 21
-    fundingBreakdownSlide,        // 22
-    teamSlide,                    // 23
-    ctaSlide,                     // 24
+    solutionSlide1,                // 3
+    solutionSlide2,                // 4
+    missionSlide,                  // 5
+    marketSlide1,                  // 6
+    marketSlide2,                  // 7
+    productSlide1,                 // 8
+    productSlide2,                 // 9
+    modulesSlide,                  // 10
+    roadmapSlide,                  // 11
+    progressSlide,                 // 12
+    platformInfrastructureSlide,   // 13
+    gtmSlide1,                     // 14
+    gtmSlide2,                     // 15
+    tokenSlide,                    // 16
+    tractionSlide,                 // 17
+    financialSlide,                // 18
+    financialModelSlide,           // 19
+    fundingNarrativeSlide,         // 20
+    fundingBreakdownSlide,         // 21
+    teamSlide,                     // 22
+    futureFinanceSlide,            // 23
+    ctaSlide,                      // 24
   ];
 
   const handleDownloadPDF = async () => {
