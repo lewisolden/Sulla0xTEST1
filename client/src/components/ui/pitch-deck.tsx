05263aadf2f669ee546b89789c8942e0117070b6
lewisolden5 <39215550-lewisolden5@users.noreply.replit.com>
2025-02-13 17:37:51 +0000

    Agent query: I've updated the pitch deck to include all slides, including the new platform infrastructure slide. Can you verify if all slides are showing up in the correct order and if you can navigate through them?
    Add new slide to pitch deck: "Future Finance".
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/978a5dde-28d4-49e6-9e38-3e475bd1f351/1dd171d8-9df9-471d-a250-0d6a4180676f.jpg

--- client/src/components/ui/pitch-deck.tsx
+++ client/src/components/ui/pitch-deck.tsx
@@ -15,6 +15,7 @@ import {
   roadmapSlide,
   progressSlide,
   platformInfrastructureSlide,
+  futureFinanceSlide,
   gtmSlide1,
   gtmSlide2,
   tokenSlide,
@@ -31,29 +32,30 @@ const PitchDeck: React.FC = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const slides = [
-    titleSlide,
-    problemSlide,
-    solutionSlide1,
-    solutionSlide2,
-    missionSlide,
-    marketSlide1,
-    marketSlide2,
-    productSlide1,
-    productSlide2,
-    modulesSlide,
-    roadmapSlide,
-    progressSlide,
-    platformInfrastructureSlide,
-    gtmSlide1,
-    gtmSlide2,
-    tokenSlide,
-    tractionSlide,
-    financialSlide,
-    financialModelSlide,
-    fundingNarrativeSlide,
-    fundingBreakdownSlide,
-    teamSlide,
-    ctaSlide,
+    titleSlide,                    // 1
+    problemSlide,                  // 2
+    solutionSlide1,               // 3
+    solutionSlide2,               // 4
+    missionSlide,                 // 5
+    marketSlide1,                 // 6
+    marketSlide2,                 // 7
+    productSlide1,                // 8
+    productSlide2,                // 9
+    modulesSlide,                 // 10
+    roadmapSlide,                 // 11
+    progressSlide,                // 12
+    platformInfrastructureSlide,  // 13
+    futureFinanceSlide,           // 14
+    gtmSlide1,                    // 15
+    gtmSlide2,                    // 16
+    tokenSlide,                   // 17
+    tractionSlide,                // 18
+    financialSlide,               // 19
+    financialModelSlide,          // 20
+    fundingNarrativeSlide,        // 21
+    fundingBreakdownSlide,        // 22
+    teamSlide,                    // 23
+    ctaSlide,                     // 24
   ];
 
   const handleDownloadPDF = async () => {
